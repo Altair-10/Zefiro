8cf20254feb1e64f3a273ff3c245171bdc7cf10f
samuele-risso <samuele.risso30@gmail.com>
2025-03-04 11:26:30 +0100

    tailwind.config modificato

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,29 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,jsx,tsx}",
+    "./src/components/**/*.{js,jsx,tsx}",
+    "./src/app/**/*.{js,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: "#000000",
+        white: "#ffffff",
+        blue: {
+          light: "#82bfca",
+          medium: "#489fb5",
+          dark: "#16697a",
+        },
+        brown: {
+          light: "#ede7e3",
+        },
+        orange: "#ffa62b",
       },
+      fontFamily: {
+        title: ["Roboto", "sans-serif"],
+        general: ["Times New Roman", "serif"],
+      },
+      fontSize: {},
     },
   },
   plugins: [],
