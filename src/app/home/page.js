bd25dd3242ddd86c9929f7673e8ae6ad665081c4
samuele-risso <samuele.risso30@gmail.com>
2025-03-06 16:17:59 +0100

    home-modificata

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Image from "next/image";
+import SocialButton from './social_button';
 
 const gridItems = [
-  { rowStart: 1, colStart: 5, colEnd: 17, color: "bg-blue-dark" },
+  { rowStart: 1, colStart: 4, colEnd: 17, color: "bg-blue-dark" },
   { rowStart: 2, colStart: 6, colEnd: 11, color: "bg-blue-light" },
   { rowStart: 2, colStart: 11, colEnd: 15, color: "bg-blue-medium" },
   { rowStart: 2, rowEnd: 5, colStart: 15, colEnd: 17, color: "bg-blue-medium" },
@@ -16,30 +17,37 @@ const gridItems = [
   { rowStart: 6, colStart: 10, color: "bg-blue-medium" },
   { rowStart: 6, colStart: 6, colEnd: 10, color: "bg-blue-dark" },
   { rowStart: 6, colStart: 11, colEnd: 17, color: "bg-blue-light" },
-  { rowStart: 7, colStart: 2, colEnd: 10, color: "bg-blue-medium" },
-  { rowStart: 7, colStart: 15, colEnd: 17, color: "bg-blue-dark" },
-  { rowStart: 8, colStart: 2, colEnd: 17, color: "bg-blue-light" },
+  { rowStart: 7, colStart: 4, colEnd: 10, color: "bg-blue-medium" },
+  { rowStart: 7, colStart: 14, colEnd: 17, color: "bg-blue-dark" },
+  { rowStart: 8, colStart: 4, colEnd: 17, color: "bg-blue-light" },
 ];
 
 export default function Page() {
   return (
     <div>
       <div className="w-screen h-[20px] bg-blue-dark" />
-        <div className="flex mt-10 ml-10 font-bold">
+      <div className="flex items-center justify-between mt-10 ml-10 font-bold max-w-[1744px]">        
+        {/* Logo + navBar */}
+        <div className="flex items-center">
           <Image 
-          src="/loghi/SmallLogoLight.png"
-          width={1000}
-          height={1000}
-          alt="Logo" 
-          className="w-32"
+            src="/loghi_altair/SmallLogoLight.png"
+            width={1000}
+            height={1000}
+            alt="Logo" 
+            className="w-32"
           />
           <button className="text-blue-medium ml-5 mt-2">SERVICE</button>
           <button className="text-blue-medium ml-5 mt-2">ABOUT US</button>
-          <button></button>
-          <button></button>
-          <button></button>
         </div>
-      <div className="flex items-center justify-center mt-5">
+
+        {/* Icone a destra */}
+        <div className="flex items-center gap-3">
+          <SocialButton source="/loghi_social/instagram.png" alt="instagram"/>
+          <SocialButton source="/loghi_social/facebook.png" alt="facebook"/>
+          <SocialButton source="/loghi_social/linkedin.png" alt="linkedin"/>
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
         <div className="grid grid-cols-[repeat(16,95px)] grid-rows-[repeat(9,60px)] mt-5 gap-2">
           {gridItems.map(({ rowStart, rowEnd, colStart, colEnd, color }, index) => (
             <div
@@ -53,8 +61,16 @@ export default function Page() {
               }}
             />
           ))}
-          <div className="row-start-2 row-end-5 col-start-1 col-end-5">
-            <h1 className="w-[500px] text-[100px] font-serif text-blue-dark">take IT for future</h1>
+          <div className="row-start-2 col-start-1 col-end-5">
+            <h1 className="w-[500px] text-[100px] font-serif text-blue-dark">take IT for</h1>
+          </div>
+          <div className="row-start-3 col-start-1 col-end-5 mt-[55px]">
+            <h1 className="w-[500px] text-[100px] font-serif text-blue-dark">future</h1>
+          </div>
+          <div className="row-start-7 col-start-1 col-end-3">
+            <div className="flex flex-center">
+              <button className="w-[200px] h-[60px] rounded-3xl bg-orange text-brown-light text-2xl">contattaci</button>
+            </div>
           </div>
         </div>
       </div>
