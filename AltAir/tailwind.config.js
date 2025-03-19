212c9b85998c4f2a25983854dd05316dd10c392d
Xive0-0 <142550222+Xive0-0@users.noreply.github.com>
2025-03-19 16:17:37 +0100

    fix

--- AltAir/tailwind.config.js
+++ AltAir/tailwind.config.js
@@ -1,7 +1,7 @@
 const {heroui, lightLayout} = require("@heroui/react");
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'class will be used' 
   content: [
     "./src/pages/**/*.{js,jsx,tsx}",
     "./src/components/**/*.{js,jsx,tsx}",
