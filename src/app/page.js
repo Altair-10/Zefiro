f70cb1efa2778515ad0509a063d6e428956e1e9b
Xive0-0 <142550222+Xive0-0@users.noreply.github.com>
2025-03-20 19:13:50 +0100

    stive_1 20-03: inserimento del preloder e dello sfondo theree.js.

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,14 @@ import Services from "./services/page";
 import AboutUs from "./aboutUs/page";
 import ContactUs from "./contactUs/page";
 import Footer from "./modules/footer";
+import ThreeBackground from "./modules/threeBackGround";
+import Preloader from "./modules/preloader";
 
 export default function SplashScreen() {
   return (
     <div className="w-screen h-screen">
+      <ThreeBackground />
+      <Preloader />
       <Navbar />
       <Home />
       <div id="servizi">
