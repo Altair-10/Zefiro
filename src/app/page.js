83dc871563d69badc75cb0f89bd442c8b4754d7d
ninuzzu5 <antoninolaferrara50@gmail.com>
2025-04-02 12:36:52 +0200

    Nino_2 02/04: Fix scrolling orizzontale navbar

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ import ChiSiamo from "./chiSiamo/chiSiamo";
 export default function SplashScreen() {
   return (
     <div>
-     <ThreeBackground /> 
+      <ThreeBackground /> 
       <Preloader />
       <Navbar />
       <div id="home">
