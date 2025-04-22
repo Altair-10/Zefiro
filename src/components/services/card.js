34d95a3bc62432d61325f17c9ef1d89fe27e4b41
ninuzzu5 <antoninolaferrara50@gmail.com>
2025-04-22 23:29:34 +0200

    Nino_05 22/04: update image and fix something

--- src/components/services/card.js
+++ src/components/services/card.js
@@ -3,7 +3,7 @@
 import React from "react";
 import { useState, useRef, useEffect } from "react"
 import Image from "next/image";
-import AnimatedButton from "./animatedButton";
+import AnimatedButton from "./AnimatedButton";
 
 const Card = ({ image, CardTitle, CardDescription, btnHref, animationDirection }) => {
   const cardRef = useRef(null);
